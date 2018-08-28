196b2195cfb581ff0f165fb3cd3dba16f1ee2a3c
Allison Dolan <allisondolan@ATM0561292DYN.dhcp.missouri.edu>
2018-08-28 18:04:07 -0500

    First Commit

---
+++ Swift Basics/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Swift Basics
+//
+//  Created by Allison Dolan on 8/28/18.
+//  Copyright © 2018 Allison Dolan. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
